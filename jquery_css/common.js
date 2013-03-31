179cd00e124f71be2224fc2747b53e2016d74742
lilclimate <lilclimate@gmail.com>
2013-03-31 14:03:01 +0800

    显示隐藏效果

---
+++ jquery_css/common.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+   var $speech = $('div.speech'); 
+   var defaultSize = $speech.css('fontSize');
+   $('#switcher button').click(function() {
+      var num = parseFloat($speech.css('fontSize')); 
+      switch(this.id) {
+        case 'switcher-large': 
+            num *= 1.4;
+            break;
+        case 'switcher-small':
+            num /= 1.4;
+            break;
+        default:
+            num = parseFloat(defaultSize);
+      }
+      $speech.css('fontSize', num + 'px');
+    });
+    
+    /*
+    //slideDown
+    $('p').eq(1).hide();
+    $('a.more').click(function() {
+       $('p').eq(1).slideDown('slow'); 
+       $(this).hide();
+       return false;
+    });
+    */
+
+    /*
+    //fadeIn/fadeOut
+    var $firstPara = $('p').eq(1);
+    $firstPara.hide();
+    $('a.more').click(function() {
+       if($firstPara.is(':hidden')) {
+          $firstPara.fadeIn('slow'); 
+          $(this).text('read less');
+        }else{
+          $firstPara.fadeOut('slow');    
+          $(this).text('read more');
+        } 
+        return false;
+    });
+    */
+
+    //slideToggle
+    /*
+    var $firstPara = $('p').eq(1);
+    $firstPara.hide();
+    $('a.more').click(function() {
+       $firstPara.slideToggle('fast'); 
+        var $link = $(this);
+       if($link.text() == 'read more') {
+          $link.text() = 'read less'; 
+       }else{
+          $link.text() = 'read more'; 
+       }
+       return false;
+    });
+    */
+});
