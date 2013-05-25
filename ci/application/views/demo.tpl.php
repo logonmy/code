955009d1c34af565aff9437719611e6064a26433
lil <lil@lil-OptiPlex-780.(none)>
2013-05-25 11:30:09 +0800

    使用bootstrap上的模板

--- ci/application/views/demo.tpl.php
+++ ci/application/views/demo.tpl.php
@@ -1,41 +1,69 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
-         <title>js</title>
-            <style type="text/css">
-            p{margin:0;font-size:12px;line-height:26px;}
-            </style>
-            <script type="text/javascript">
-            function check_all(obj,cName) {
-                var checkboxs = document.getElementsByName(cName);     
-                for(var i=0;i<checkboxs.length;i++) {
-                   checkboxs[i].checked = obj.checked; 
-                }
-            }
+        <meta charset="utf-8">
+        <title>Bootstrap, from Twitter</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-            function checkall(Name) {
-               var checkboxs = document.getElementsByName(Name); 
-               for(var i=0;i<checkboxs.length;i++) {
-                  checkboxs[i].checked = true; 
-               }
+        <!-- Le styles -->
+        <link href="<?php echo $demo_config['site']['static_path'];?>/css/bootstrap.min.css" rel="stylesheet">
+        <script src="<?php echo $demo_config['site']['static_path'];?>/js/jquery-1.9.1.min.js"></script>
+        <script src="<?php echo $demo_config['site']['static_path'];?>/js/bootstrap.js"></script>
+        <style>
+            body {
+                padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
             }
+        </style>
 
-            function check_none(Name) {
-               var checkboxs = document.getElementsByName(Name); 
-               for(var i=0;i<checkboxs.length;i++) {
-                  checkboxs[i].checked = false; 
-               }
-            }
-            </script>
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+              <script src="//cdnjs.bootcss.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+                  <![endif]-->
+
+        <!-- Fav and touch icons -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+        <link rel="shortcut icon" href="../assets/ico/favicon.png">
     </head>
+
     <body>
-    <p><input type="checkbox" name="all" onclick="check_all(this,'c')"/>全选/全不选</p>
-    <p><button onclick="checkall('c')">全选</button></p>
-    <p><button onclick="check_none('c')">取消</button></p>
-    <p><input type="checkbox" name="c" value=""/></p>
-    <p><input type="checkbox" name="c" value=""/></p>
-    <p><input type="checkbox" name="c" value=""/></p>
-    <p><input type="checkbox" name="c" value=""/></p>
-    <p><input type="checkbox" name="c" value=""/></p>
+
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="brand" href="#">Project name</a>
+                    <div class="nav-collapse collapse">
+                        <ul class="nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+
+            <h1>Bootstrap starter template</h1>
+            <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+
+        </div> <!-- /container -->
+
+        <!-- Le javascript
+            ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="//cdnjs.bootcss.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="//cdnjs.bootcss.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+
     </body>
 </html>
