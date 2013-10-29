9b373c922743d3f05406df84f366cbaaea00e47c
lil <lil@lil.(none)>
2013-10-30 00:15:38 +0800

    增加模块

---
+++ hmvc_demo/application/modules_core/hmvc/controllers/hmvc.php
@@ -0,0 +1,7 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+class Hmvc extends MX_Controller {
+    
+    public function index() {
+        echo "hmvc";exit;    
+    }
+}
